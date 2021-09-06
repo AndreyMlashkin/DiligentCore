3af8ba0014396ffe73d3b8634918d03b8ac1901e
Andrei Malashkin <andrei.malashkin@reactivereality.com>
2021-09-06 21:12:41 +0200

    use vulkan and volk from conan

--- Graphics/GraphicsEngineVulkan/include/VulkanUtilities/VulkanHeaders.h
+++ Graphics/GraphicsEngineVulkan/include/VulkanUtilities/VulkanHeaders.h
@@ -37,7 +37,7 @@
 #define VK_FORMAT_RANGE_SIZE (VK_FORMAT_ASTC_12x12_SRGB_BLOCK - VK_FORMAT_UNDEFINED + 1)
 
 #if DILIGENT_USE_VOLK
-#    include "volk/volk.h"
+#    include "volk.h"
 #endif
 
 #ifdef _WINBASE_
